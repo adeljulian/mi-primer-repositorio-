6f6e01d7a3e8caa79943b6329ecd7929e942404a
adeljulian <adelsandoval02@gmail.com>
2026-04-26 00:28:57 -0500

    Add files via upload

---
+++ variables.py
@@ -0,0 +1,17 @@
+#inicializando variables y colocandoles un valor
+nombre="adel julian meza sandoval"
+edad=13
+estado=True
+nota=5.0
+
+#Esta linea me imprime el contenido de una variable
+print("bienvenido",nombre)
+print("tu edad es",edad)
+
+#Uso de la funcion input()
+nombre=input("¿Cual es tu nombre? ")
+edad=input("¿Cual es tu edad? ")
+
+#Mostrar el resultado capturado con input()
+print("Bienvenido", nombre)
+print("Tu edad es", edad)
